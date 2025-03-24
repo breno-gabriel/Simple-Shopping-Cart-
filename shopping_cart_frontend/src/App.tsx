6ba0ceda30da5505bdbdd8c64e22b19c86b81d43
Breno <brenomelogabriel2019@gmail.com>
2025-03-24 14:44:43 -0300

    feat: adding header

--- shopping_cart_frontend/src/App.tsx
+++ shopping_cart_frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import ProductCard from "./components/ProductCard"
+import Header from "./components/Header"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-red-500">Hello World</h1>
-    </>
+    <div className="flex flex-col items-center gap-20 justify-center min-h-screen px-20">
+      <Header></Header>
+      <div className="flex flex-wrap gap-5">
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+      </div>
+    </div>
   )
 }
 
---
+++ shopping_cart_frontend/src/components/Header.tsx
@@ -0,0 +1,47 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { CiLight } from "react-icons/ci";
+
+const Header = () => {
+    return (
+        <header className="flex justify-between w-full pr-6 py-8 font-bold">
+            <h1 className="text-3xl">
+                <span className="text-[#411DE2]">V(</span>Tech<span className="text-[#411DE2]">)</span>
+            </h1>
+
+            <div className="flex gap-10">
+                <nav>
+                    <ul className="flex gap text-3xl gap-10">
+                        <li>
+                            <a 
+                                href="#home" 
+                                className="hover:text-blue-500 hover:underline transition duration-300"
+                            >
+                                home
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="#store" 
+                                className="hover:text-blue-500 hover:underline transition duration-300"
+                            >
+                                store
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                                href="#about" 
+                                className="hover:text-blue-500 hover:underline transition duration-300"
+                            >
+                                about
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <FaShoppingCart className="text-4xl cursor-pointer" />
+                <CiLight className="text-4xl cursor-pointer" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
